c48134ab0d741e56d1a4b6476c2e6baece328f86
axelweyers <axelweyers@gmail.com>
2025-12-06 11:35:29 +0100

    add stap 3

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from typing import Optional
 class Afspraak(BaseModel):
+    id:Optional[int]=None
     titel:str
     datum:str
 
 app = FastAPI()
-data=[
-        {"titel":"dokter",
-         "jaar":"2000",
-         "maand":"july",
-         "dag":"8"},
-        {"titel":"school",
-         "jaar":"2000",
-         "maand":"july",
-         "dag":"8"},
-        {"titel":"werk",
-         "jaar":"2000",
-         "maand":"july",
-         "dag":"8"}
-     ]
-
+data=[]
+id=1
 
 @app.get("/")
 async def root():
     return data
 @app.post("/")
 async def create_item(afspraak:Afspraak):
+    global id
+    afspraak.id=id
+    id+=1
     data.append(afspraak)
     return afspraak
+@app.delete("/")
+async def delete_item(id_item:int):
+    for index, item in enumerate(data):
+        if item.id == id_item:
+            data.pop(index)
+    return "verwijdert"
