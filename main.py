f80238f147981242862eacb0d67655812844ca9b
axelweyers <axelweyers@gmail.com>
2025-12-06 10:49:03 +0100

    update stap 1

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+data=[
+        {"titel":"dokter",
+         "jaar":"2000",
+         "maand":"july",
+         "dag":"8"},
+        {"titel":"school",
+         "jaar":"2000",
+         "maand":"july",
+         "dag":"8"},
+        {"titel":"werk",
+         "jaar":"2000",
+         "maand":"july",
+         "dag":"8"}
+     ]
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return data
